a831185caedd65a2752b241bedf50b5e73d8da20
dducktai <dangductai6410@gmail.com>
2024-05-21 00:57:13 +0700

    Lab4 task3

--- Lab4/Task3.Designer.cs
+++ Lab4/Task3.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.rtbHTML = new System.Windows.Forms.RichTextBox();
+            this.txtURL = new System.Windows.Forms.TextBox();
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // label1
@@ -42,11 +46,50 @@
             this.label1.Text = "TASK 3";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
+            // rtbHTML
+            // 
+            this.rtbHTML.Location = new System.Drawing.Point(44, 153);
+            this.rtbHTML.Name = "rtbHTML";
+            this.rtbHTML.ReadOnly = true;
+            this.rtbHTML.Size = new System.Drawing.Size(706, 386);
+            this.rtbHTML.TabIndex = 7;
+            this.rtbHTML.Text = "";
+            // 
+            // txtURL
+            // 
+            this.txtURL.Location = new System.Drawing.Point(44, 74);
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(570, 26);
+            this.txtURL.TabIndex = 6;
+            this.txtURL.Text = "https://uit.edu.vn/";
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Location = new System.Drawing.Point(632, 74);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(119, 58);
+            this.btnDownload.TabIndex = 5;
+            this.btnDownload.Text = "Download";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(44, 106);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(570, 26);
+            this.txtPath.TabIndex = 8;
+            this.txtPath.Text = "C:\\Users\\Admin\\Downloads\\";
+            // 
             // Task3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 551);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.rtbHTML);
+            this.Controls.Add(this.txtURL);
+            this.Controls.Add(this.btnDownload);
             this.Controls.Add(this.label1);
             this.Name = "Task3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -59,5 +102,9 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbHTML;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox txtPath;
     }
 }
--- Lab4/Task3.cs
+++ Lab4/Task3.cs
@@ -3,7 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +18,16 @@ namespace Lab4
         public Task3()
         {
             InitializeComponent();
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            WebClient myClient = new WebClient();
+            Stream response = myClient.OpenRead(txtURL.Text);
+            myClient.DownloadFile(txtURL.Text, txtPath.Text);
+
+            string content = new StreamReader(response).ReadToEnd();
+            rtbHTML.Text = content;
         }
     }
 }
